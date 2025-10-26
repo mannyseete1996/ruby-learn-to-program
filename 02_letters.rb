239b37c8d41ca6253a3098e0fe9c88e8a891cbf9
Emman <39931114+mannyseete1996@users.noreply.github.com>
2025-10-26 13:58:32 +0800

    Add additional comments

--- 02_letters.rb
+++ 02_letters.rb
@@ -1,29 +1,29 @@
 # frozen_string_literal: true
 
-puts 'Hello, world!'
-puts ''
-puts 'Good-bye'
+puts 'Hello, world!' # Hello, world!
+puts '' # Empty string
+puts 'Good-bye' # Good-bye
 
 
 # String Arithmetic
 
-puts 'I like ' + 'apple pie.'
+puts 'I like ' + 'apple pie.' # I like apple pie.
 
-puts 'blink ' * 4
+puts 'blink ' * 4 # blink blink blink blink
 
 
 # 12 vs '12'
 
-puts 12 + 12
-puts '12' + '12'
-puts '12 + 12'
+puts 12 + 12 # 24
+puts '12' + '12' # 1212
+puts '12 + 12' # 12 + 12
 
-puts 2 * 5
-puts '2' * 5
-puts '2 * 5'
+puts 2 * 5 # 10
+puts '2' * 5 # 22222
+puts '2 * 5' # 2 * 5
 
 
-puts 'You\'re swell!'
-puts 'backslash at the end of a string:  \\'
-puts 'up\\down'
-puts 'up\down'
+puts 'You\'re swell!' # You're swell!
+puts 'backslash at the end of a string:  \\' # backslash at the end of a string:  \
+puts 'up\\down' # up\down
+puts 'up\down' # up\down
