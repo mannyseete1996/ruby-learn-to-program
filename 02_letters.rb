4aeb7986489ddaac49164c38feef07acc65d1084
Emman <39931114+mannyseete1996@users.noreply.github.com>
2025-10-26 14:39:32 +0800

    Update 02_letters.rb

--- 02_letters.rb
+++ 02_letters.rb
@@ -1,29 +1,50 @@
 # frozen_string_literal: true
 
-puts 'Hello, world!' # Hello, world!
-puts '' # Empty string
-puts 'Good-bye' # Good-bye
+# Hello, world!
+puts 'Hello, world!'
+
+puts ''
+
+# Good-bye
+puts 'Good-bye'
 
 
 # String Arithmetic
 
-puts 'I like ' + 'apple pie.' # I like apple pie.
+# I like apple pie.
+puts 'I like ' + 'apple pie.'
 
-puts 'blink ' * 4 # blink blink blink blink
-
+# blink blink blink blink
+puts 'blink ' * 4
 
 # 12 vs '12'
 
-puts 12 + 12 # 24
-puts '12' + '12' # 1212
-puts '12 + 12' # 12 + 12
+# 24
+puts 12 + 12
 
-puts 2 * 5 # 10
-puts '2' * 5 # 22222
-puts '2 * 5' # 2 * 5
+# 1212
+puts '12' + '12'
 
+# 12 + 12
+puts '12 + 12'
 
-puts 'You\'re swell!' # You're swell!
-puts 'backslash at the end of a string:  \\' # backslash at the end of a string:  \
-puts 'up\\down' # up\down
-puts 'up\down' # up\down
+# 10
+puts 2 * 5
+
+# 22222
+puts '2' * 5
+
+# 2 * 5
+puts '2 * 5'
+
+# You're swell!
+puts 'You\'re swell!'
+
+# backslash at the end of a string:  \
+puts 'backslash at the end of a string:  \\'
+
+# up\down
+puts 'up\\down'
+
+# up\down
+puts 'up\down'
