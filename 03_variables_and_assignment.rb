ff2b09e2d5d4152ed646ed0a09c9a78d54f462e3
Emman <39931114+mannyseete1996@users.noreply.github.com>
2025-10-26 14:08:01 +0800

    Create 03_variables_and_assignment.rb

---
+++ 03_variables_and_assignment.rb
@@ -0,0 +1,22 @@
+# frozen_string_literal: true
+
+my_string = '...you can say that again...' # Assign a string to my_string
+puts my_string # Print the string
+puts my_string # Print the string again
+
+composer = 'Mozart' # Assign a string to composer
+puts "#{composer} was \"da bomb\", in his day." # Print the string with composer's name
+
+composer = 'Beethoven' # Assign a string to composer
+puts "But I prefer #{composer}, personally." # Print the string with composer's name
+
+var1 = 8 # Assign a number to var1
+var2 = var1 # Assign var1 to var2
+puts var1 # Print the value of var1
+puts var2 # Print the value of var2
+
+puts '' # Print an empty line
+
+var1 = 'eight' # Assign a string to var1
+puts var1 # Print the value of var1
+puts var2 # Print the value of var2
