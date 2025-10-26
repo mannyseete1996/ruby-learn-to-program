c326814ef5ca165b364f335663293ab11b25e287
Emman <39931114+mannyseete1996@users.noreply.github.com>
2025-10-26 14:40:39 +0800

    Update 03_variables_and_assignment.rb

--- 03_variables_and_assignment.rb
+++ 03_variables_and_assignment.rb
@@ -1,22 +1,45 @@
 # frozen_string_literal: true
 
-my_string = '...you can say that again...' # Assign a string to my_string
-puts my_string # Print the string
-puts my_string # Print the string again
+# Assign a string to my_string
+my_string = '...you can say that again...'
 
-composer = 'Mozart' # Assign a string to composer
-puts "#{composer} was \"da bomb\", in his day." # Print the string with composer's name
+# Print the string
+puts my_string
 
-composer = 'Beethoven' # Assign a string to composer
-puts "But I prefer #{composer}, personally." # Print the string with composer's name
+# Print the string again
+puts my_string
 
-var1 = 8 # Assign a number to var1
-var2 = var1 # Assign var1 to var2
-puts var1 # Print the value of var1
-puts var2 # Print the value of var2
+# Assign a string to composer
+composer = 'Mozart'
 
-puts '' # Print an empty line
+# Print the string with composer's name
+puts "#{composer} was \"da bomb\", in his day."
 
-var1 = 'eight' # Assign a string to var1
-puts var1 # Print the value of var1
-puts var2 # Print the value of var2
+# Assign a string to composer
+composer = 'Beethoven'
+
+# Print the string with composer's name
+puts "But I prefer #{composer}, personally."
+
+# Assign a number to var1
+var1 = 8
+
+# Assign var1 to var2
+var2 = var1
+
+# Print the value of var1
+puts var1
+
+# Print the value of var2
+puts var2
+
+puts ''
+
+# Assign a string to var1
+var1 = 'eight'
+
+# Print the value of var1
+puts var1
+
+# Print the value of var2
+puts var2
