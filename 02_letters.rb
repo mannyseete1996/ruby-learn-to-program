2e445174c27a4b56bd95ab0fc8a207ac166229a2
Emman <39931114+mannyseete1996@users.noreply.github.com>
2025-10-26 13:57:02 +0800

    Create 02_letters.rb

---
+++ 02_letters.rb
@@ -0,0 +1,29 @@
+# frozen_string_literal: true
+
+puts 'Hello, world!'
+puts ''
+puts 'Good-bye'
+
+
+# String Arithmetic
+
+puts 'I like ' + 'apple pie.'
+
+puts 'blink ' * 4
+
+
+# 12 vs '12'
+
+puts 12 + 12
+puts '12' + '12'
+puts '12 + 12'
+
+puts 2 * 5
+puts '2' * 5
+puts '2 * 5'
+
+
+puts 'You\'re swell!'
+puts 'backslash at the end of a string:  \\'
+puts 'up\\down'
+puts 'up\down'
