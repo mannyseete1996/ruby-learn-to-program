d8dd803ea4e42b1d72277637559e38278191e1f2
Emman <39931114+mannyseete1996@users.noreply.github.com>
2025-10-28 17:17:02 +0800

    Create 05_more_about_methods.rb

---
+++ 05_more_about_methods.rb
@@ -0,0 +1,16 @@
+# frozen_string_literal: true
+
+iCantBelieveIMadeAVariableNameThisLongJustToPointToA3 = 3
+puts iCantBelieveIMadeAVariableNameThisLongJustToPointToA3
+self.puts iCantBelieveIMadeAVariableNameThisLongJustToPointToA3
+
+var1 = 'stop'
+var2 = 'stressed'
+var3 = 'Can you pronounce this sentence backwards?'
+
+puts var1.reverse
+puts var2.reverse
+puts var3.reverse
+puts var1
+puts var2
+puts var3
