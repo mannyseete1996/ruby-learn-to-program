8c69808d1fd8971e1a5f40f7bf3ea83f5445411c
Emman <39931114+mannyseete1996@users.noreply.github.com>
2025-10-28 18:45:53 +0800

    Update 05_more_about_methods.rb

--- 05_more_about_methods.rb
+++ 05_more_about_methods.rb
@@ -1,8 +1,8 @@
 # frozen_string_literal: true
 
-iCantBelieveIMadeAVariableNameThisLongJustToPointToA3 = 3
-puts iCantBelieveIMadeAVariableNameThisLongJustToPointToA3
-self.puts iCantBelieveIMadeAVariableNameThisLongJustToPointToA3
+i_cant_believe_i_made_a_variable_name_this_long_just_to_point_to_a_3 = 3
+puts i_cant_believe_i_made_a_variable_name_this_long_just_to_point_to_a_3
+self.puts i_cant_believe_i_made_a_variable_name_this_long_just_to_point_to_a_3
 
 var1 = 'stop'
 var2 = 'stressed'
@@ -14,3 +14,17 @@ puts var3.reverse
 puts var1
 puts var2
 puts var3
+
+puts 'What is your full name?'
+name = gets.chomp
+puts 'Did you know there are ' + name.length.to_s + ' characters in your name, ' + name + '?'
+
+letters = 'aAbBcCdDeE'
+puts letters.upcase
+puts letters.downcase
+puts letters.swapcase
+puts letters.capitalize
+puts ' a'.capitalize
+puts letters
+
+lineWidth = 50
